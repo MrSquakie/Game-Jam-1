e536ede08ecf9af7062b716b019b5d00ad92ad5a
Allen <49079375+ajfoust@users.noreply.github.com>
2019-09-17 18:15:55 -0700

    Initial

---
+++ Game Jam 1/Assets/Scripts/CreateObjecctonShoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObjecctonShoot : MonoBehaviour
+{
+    public Shooting shootingVars;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shootingVars = GetComponent<Shooting>();
+        shootingVars.Clicked();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
