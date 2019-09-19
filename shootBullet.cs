81700a45a63e523ba0cee806807954d46a86da90
josephuang0327 <55466018+josephuang0327@users.noreply.github.com>
2019-09-18 22:28:34 -0700

    Add files via upload

---
+++ shootBullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootBullet : MonoBehaviour
+{
+
+    public GameObject projectile;
+    public float bulletSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Clicked();
+    }
+
+    public void Clicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+
+        }
+
+    }
+
+}
