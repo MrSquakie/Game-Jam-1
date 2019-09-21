50ebfe57b03f484175f834017605a149ee20e857
itsFreakinSef <sefreif@gmail.com>
2019-09-20 17:21:30 -0700

    Enemy Movement
    Prefabbed all of my test objects if you wanted to test before implimenting. EnemyNav is where the entire movement script is located.

---
+++ Assets/SefTest/EnemyNav.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNav : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject goal;
+    public NavMeshAgent agent;
+    public Shooting hit; //doesn't work
+    private bool follow;
+    private void OnTriggerEnter(Collider other)
+    {
+        //when enemy runs collides with player/castle, enemy stops
+        if(player)
+        {
+            agent.isStopped = true;
+        }
+
+        if(goal)
+        {
+            agent.isStopped = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(player)
+        {
+            //Attack
+        }
+
+        if(goal)
+        {
+            //Attack
+        }
+    }
+    private void OnTriggerExit(Collider player)
+    {
+        //if player moves away from enemy after colliding, enemy follows
+        follow = true;
+        agent.isStopped = false;
+    }
+    void Update()
+    {
+        
+        /*if (hit)
+        {
+            follow = true;
+        }*/ //doesn't work
+
+        if(follow == true)
+        {
+            //follows player
+            Vector3 playerPos = player.transform.position;
+            agent.SetDestination(playerPos);
+        }
+        else
+        {
+            //moves to castle
+            Vector3 goalPos = goal.transform.position;
+            agent.SetDestination(goalPos);
+        }
+        
+    }
+}
