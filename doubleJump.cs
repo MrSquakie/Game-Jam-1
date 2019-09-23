e6fa0b912807e4c7439da3164c9954b4533b45ef
josephuang0327 <55466018+josephuang0327@users.noreply.github.com>
2019-09-22 17:55:22 -0700

    Add files via upload

---
+++ doubleJump.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doubleJump : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    public int maxJump = 2;
+    int jumps;
+
+    public float moveSpeed = 10f;
+    public float jumpforce = 10f;
+
+    public bool grounded;
+
+
+
+    void Start()
+    {
+        jumps = maxJump;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            Jump();
+        }
+    }
+    void Jump()
+    {
+        if (jumps > 0)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+            grounded = false;
+            jumps = jumps - 1;
+        }
+        if (jumps == 0)
+        {
+            return;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collide)
+    {
+        if (collide.gameObject.tag == "Ground")
+        {
+            jumps = maxJump;
+            grounded = true;
+            moveSpeed = 2.0F;
+        }
+    }
+   
+    
+      void GoUp()
+    {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+        jumps = jumps - 1;
+    }
+
+}
