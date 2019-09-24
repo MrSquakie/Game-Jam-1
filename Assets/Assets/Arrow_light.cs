86f1a5212ddf3b4dbbb53e147fa52536a327128e
MrSquakie <45904658+MrSquakie@users.noreply.github.com>
2019-09-23 23:38:27 -0700

    added scenes

---
+++ Assets/Assets/Arrow_light.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow_light : MonoBehaviour
+{
+    public Light light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = gameObject.AddComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/shootBullet.cs
+++ Assets/Scripts/shootBullet.cs
@@ -7,6 +7,7 @@ public class shootBullet : MonoBehaviour
     public GameObject bulletEmitter;
     public float bulletSpeed = 10;
     public GameObject projectile;
+    public GameObject lightProjectile;
     public GameObject target;
 	static Animator anim;
     // Start is called before the first frame update
@@ -44,6 +45,23 @@ public class shootBullet : MonoBehaviour
 		{
 			anim.SetBool("Fire",false);
 		}
+        if (Input.GetMouseButtonDown(1))
+        {
+            anim.SetBool("Fire", true);
+            GameObject temporary_bullet_handler;
+            temporary_bullet_handler = Instantiate(lightProjectile, bulletEmitter.transform.position, bulletEmitter.transform.rotation) as GameObject;
+            temporary_bullet_handler.transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward, Camera.main.transform.up) * Quaternion.Euler(90f, 0f, 0f);
+            //temporary_bullet_handler.transform.Rotate(Vector3.left * 90);
+
+            Rigidbody temporary_rigidbody;
+            temporary_rigidbody = temporary_bullet_handler.GetComponent<Rigidbody>();
+            temporary_rigidbody.velocity = Camera.main.transform.forward * 60f;
+
+            Destroy(temporary_bullet_handler, 10.0f);
+            //GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            // bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+
+        }
 
     }
 
